a9e7a209afd4aed70db80058473f76a22431b686
Michael Baker <michael.a.baker34@gmail.com>
2021-02-26 13:51:37 -0600

    add getMovie function

--- js/script.js
+++ js/script.js
@@ -6,4 +6,11 @@ const apiUrl = "https://utopian-absorbing-network.glitch.me/movies";
         .then(response => response.json())
         .catch(console.error);
 
-    console.log(getMovies());
+    // console.log(getMovies());
+
+    const getMovie = id => fetch(`${apiUrl}/${id}`)
+        .then(response => response.json())
+        .catch(console.error);
+
+    console.log(getMovie(1));
+    // console.log(getMovie(2));
